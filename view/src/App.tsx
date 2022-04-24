c8866c007b6ab63369b307fea49e98a4436b08dc
Thiago Buarque <thiago.buarqque@gmail.com>
2022-04-24 11:14:28 -0300

    customers CRUD done

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import CustomersPage from "./pages/Customers";
+import OrdersPage from "./pages/Orders";
 
-import "./App.css";
+import "./styles/App.scss";
+
 const App: React.FC = () => {
-    return <div className="App">Northwind</div>;
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route path="/orders" element={<OrdersPage />} />
+                <Route path="/customers" element={<CustomersPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
